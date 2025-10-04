7f95aa8673a85f717f01d29bc8a5de775f2fa6f0
Adriano Abdelnur <adrianoabdelnur@hotmail.com>
2025-10-04 18:04:27 -0300

    padron added in router

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.use('/user', require('./user'))
+router.use('/padron', require('./padron'))
 
 
 module.exports = router; 
