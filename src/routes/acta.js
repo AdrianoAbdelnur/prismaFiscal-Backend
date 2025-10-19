687efdb0436c1e48112ba1281c070b7d30cf9f4c
Adriano Abdelnur <adrianoabdelnur@hotmail.com>
2025-10-19 11:45:46 -0300

    getActaByMesa added

--- src/routes/acta.js
+++ src/routes/acta.js
@@ -8,6 +8,7 @@ const {
   closeActa,
   getAllVersions,
   getTotalsByParty,
+  getActaByMesa,
 } = require("../controllers/acta");
 
 router.post("/upsertActa", upsertActa);
@@ -23,5 +24,8 @@ router.patch("/:mesaId/close", closeActa);
 router.get("/:mesaId/versions", getAllVersions);
 
 router.get("/totales", getTotalsByParty);
+
+router.get("/mesa/:mesaId", getActaByMesa); 
+router.get("/varias", getActaByMesa);
 
 module.exports = router;
